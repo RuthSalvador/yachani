83f82b9f116a41f7f4dae3e55e04a15fe3153996
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-30 08:33:08 -0500

    quitando simbolos merging

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-var preguntas=[{question:"¿Que podía curar al rey?",type:"factual",alternatives:["Nada podía curarlo.","El agua.","El agua de la vida."]},
-               {question:"¿Por qué crees que el duendecillo no se molestó por su indeferencia, ya que el príncipe se disculpó?",type:"factual",alternatives:["El duendecillo tenía una maldición que no podía molestarse si escuchaba la palabra disculpa.","La disculpa es símbolo de respeto y buenos modales.","No puedes molestarte con nadie si te pide disculpas."]},
-               {question:"¿Que debió haber hecho el príncipe?",type:"factual",alternatives:["Llevar ayuda.","Preguntar ¿qué hay en el castillo?","Ambas."]},
-               {question:"En el texto la palabra “garganta”, ¿qué significado tiene?",type:"léxica",alternatives:["Parte frontal del cuello.","Paso estrecho entre montañas.","Joya típica de la orfebrería precolombina."]},
-               {question:"¿De qué manera el príncipe insultó al duende?",type:"inferencial",alternatives:["Molesto","Enano","Tonto"]},
-               {question:"¿Cómo llegó el principe al castillo?",type:"factual",alternatives:["Con un hechizo del duende.","A pie.","Cabalgando."]},
-               {question:"¿Qué hubiera pasado si el príncipe no hubiera salido antes de las 12 del castillo?",type:"inferencial",alternatives:["Hubiera llegado tarde a ver a su padre","Se quedaría una semana más en el castillo","Se quedaría por siempre en el castillo"]}
-               ];
-
-{question:"Qué haces ante la pregunta del pequeño duendecillo?",alternatives:["Te disculpas por no poder escucharlo, le dices que estás apurado por una urgencia familiar.","Le  dices que no te importa, que estás apurado.","Le explicas lo que ha pasado , tu misión y pides su ayuda."]},
-=======
-=======
 var slideIndex = 1;
->>>>>>> c16c08d7a32d10192a4722d02f1611db9fa2ca93
 var contenedor = document.getElementById('galeria');
 var docFragment=document.createDocumentFragment();
 var option = document.getElementById('literal').addEventListener('click',function(e){
@@ -220,9 +204,6 @@ function crearAlternativas(alternativa,id){
 
   return div;
 }
-<<<<<<< HEAD
->>>>>>> f5a4206751eb9b84d7d42e26d70b7f80eaa4b086
-=======
 
 function plusSlide(n) {
   showSlide(slideIndex += n);
@@ -247,4 +228,3 @@ function showSlide(index){
   //   e.preventDefault();
   //     plusSlide(1);
   // });
->>>>>>> c16c08d7a32d10192a4722d02f1611db9fa2ca93
